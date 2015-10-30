77532e849411965d2a7631ce6bbc20dc16ffdf85
baichengzhang <baichengzhang@gmail.com>
2015-10-30 17:06:32 -0400

    add ball game

---
+++ app/src/main/java/nyu/tandon/cs9033/gameralarm/controllers/MainActivity.java
@@ -0,0 +1,17 @@
+package nyu.tandon.cs9033.gameralarm.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import nyu.tandon.cs9033.gameralarm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, BallGameActivity.class);
+        startActivity(intent);
+    }
+}
